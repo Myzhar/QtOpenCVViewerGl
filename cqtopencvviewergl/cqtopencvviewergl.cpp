db6b2df996df91e77aca572b8f2e654d85b2c440
Agris Dumins <agris.dumins@gmail.com>
2016-06-12 22:30:43 +0300

    Fast fixes for fliped image, artifacts from glDrawPixels use, widget resize / update.

--- cqtopencvviewergl/cqtopencvviewergl.cpp
+++ cqtopencvviewergl/cqtopencvviewergl.cpp
@@ -1,70 +1,44 @@
 #include "cqtopencvviewergl.h"
 
 #include <QOpenGLFunctions>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
 CQtOpenCVViewerGl::CQtOpenCVViewerGl(QWidget *parent) :
-    QOpenGLWidget(parent)
+QOpenGLWidget(parent)
 {
-    mSceneChanged = false;
     mBgColor = QColor::fromRgb(150, 150, 150);
-
-    mOutH = 0;
-    mOutW = 0;
-    mImgRatio = 4.0f/3.0f;
-
-    mPosX = 0;
-    mPosY = 0;
 }
 
 void CQtOpenCVViewerGl::initializeGL()
 {
     makeCurrent();
     initializeOpenGLFunctions();
-
+    
     float r = ((float)mBgColor.darker().red())/255.0f;
     float g = ((float)mBgColor.darker().green())/255.0f;
     float b = ((float)mBgColor.darker().blue())/255.0f;
     glClearColor(r,g,b,1.0f);
-
-    // qglClearColor(mBgColor.darker()); obsolete
 }
 
 void CQtOpenCVViewerGl::resizeGL(int width, int height)
 {
     makeCurrent();
     glViewport(0, 0, (GLint)width, (GLint)height);
-
+    
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
-    glOrtho(0, width, 0, height, 0, 1);	// To Draw image in the center of the area
-
+    
+    glOrtho(0, width, -height, 0, 0, 1);
+    
     glMatrixMode(GL_MODELVIEW);
-
-    // ---> Scaled Image Sizes
-    mOutH = width/mImgRatio;
-    mOutW = width;
-
-    if(mOutH>height)
-    {
-        mOutW = height*mImgRatio;
-        mOutH = height;
-    }
-
-    emit imageSizeChanged( mOutW, mOutH );
-    // <--- Scaled Image Sizes
-
-    mPosX = (width-mOutW)/2;
-    mPosY = (height-mOutH)/2;
-
-    mSceneChanged = true;
-
+    
     updateScene();
 }
 
 void CQtOpenCVViewerGl::updateScene()
 {
-    if( mSceneChanged && this->isVisible() )
+    if( this->isVisible() )
         //updateGL(); obsolete
         update();
 }
@@ -72,89 +46,73 @@ void CQtOpenCVViewerGl::updateScene()
 void CQtOpenCVViewerGl::paintGL()
 {
     makeCurrent();
-
-    if( !mSceneChanged )
-        return;
-
+    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    
     renderImage();
-
-    mSceneChanged = false;
 }
 
 void CQtOpenCVViewerGl::renderImage()
 {
     makeCurrent();
-
+    
     glClear(GL_COLOR_BUFFER_BIT);
-
+    
     if (!mRenderQtImg.isNull())
     {
         glLoadIdentity();
-
+        
         QImage image; // the image rendered
-
+        
         glPushMatrix();
         {
-            int imW = mRenderQtImg.width();
-            int imH = mRenderQtImg.height();
-
-            // The image is to be resized to fit the widget?
-            if( imW != this->size().width() &&
-                    imH != this->size().height() )
-            {
-
-                image = mRenderQtImg.scaled( //this->size(),
-                                             QSize(mOutW,mOutH),
-                                             Qt::IgnoreAspectRatio,
-                                             Qt::SmoothTransformation
-                                             );
-
-                //qDebug( QString( "Image size: (%1x%2)").arg(imW).arg(imH).toAscii() );
+            // calculate
+            int nw = this->size().width();
+            int nh = floor(nw / mImgRatio);
+            
+            if (nh > this->size().height()){
+                nh = this->size().height();
+                nw = floor(nh * mImgRatio);
             }
-            else
+            
+            if (nw == mRenderQtImg.width() && nh == mRenderQtImg.height())
                 image = mRenderQtImg;
-
+            else
+                image = mRenderQtImg.scaled(QSize(nw, nh),
+                                            Qt::IgnoreAspectRatio,
+                                            Qt::SmoothTransformation);
+        
             // ---> Centering image in draw area
-
-            glRasterPos2i( mPosX, mPosY );
-            // <--- Centering image in draw area
-
-            imW = image.width();
-            imH = image.height();
-
-            glDrawPixels( imW, imH, GL_RGBA, GL_UNSIGNED_BYTE, image.bits());
+            
+            glRasterPos2i(floor((this->size().width() - image.width()) / 2),
+                          -floor((this->size().height() - image.height()) / 2));
+            
+            glPixelZoom(1, -1);
+            
+            glDrawPixels( image.width(), image.height(), GL_RGBA, GL_UNSIGNED_BYTE, image.bits());
         }
         glPopMatrix();
-
+        
         // end
         glFlush();
     }
 }
 
-bool CQtOpenCVViewerGl::showImage(cv::Mat image)
+bool CQtOpenCVViewerGl::showImage(const cv::Mat& image)
 {
-    image.copyTo(mOrigImage);
-
     mImgRatio = (float)image.cols/(float)image.rows;
-
-    if( mOrigImage.channels() == 3)
-        mRenderQtImg = QImage((const unsigned char*)(mOrigImage.data),
-                              mOrigImage.cols, mOrigImage.rows,
-                              mOrigImage.step, QImage::Format_RGB888)/*.rgbSwapped()*/;
-    else if( mOrigImage.channels() == 1)
-        mRenderQtImg = QImage((const unsigned char*)(mOrigImage.data),
-                              mOrigImage.cols, mOrigImage.rows,
-                              mOrigImage.step, QImage::Format_Indexed8);
-    else
-        return false;
-
-    //mRenderQtImg = QGLWidget::convertToGLFormat(mRenderQtImg); obsolete
-
-    mSceneChanged = true;
-
+    
+    if (image.channels() == 3)
+        cvtColor(image, mOrigImage, CV_BGR2RGBA);
+    else if (mOrigImage.channels() == 1)
+        cvtColor(image, mOrigImage, CV_GRAY2RGBA);
+    else return false;
+    
+    mRenderQtImg = QImage((const unsigned char*)(mOrigImage.data),
+                          mOrigImage.cols, mOrigImage.rows,
+                          mOrigImage.step1(), QImage::Format_RGB32);
+    
     updateScene();
-
+    
     return true;
 }
--- cqtopencvviewergl/cqtopencvviewergl.h
+++ cqtopencvviewergl/cqtopencvviewergl.h
@@ -10,36 +10,29 @@ class CQtOpenCVViewerGl : public QOpenGLWidget, protected QOpenGLFunctions_2_0
     Q_OBJECT
 public:
     explicit CQtOpenCVViewerGl(QWidget *parent = 0);
-
+    
 signals:
     void    imageSizeChanged( int outW, int outH ); /// Used to resize the image outside the widget
-
-public slots:
-    bool    showImage(cv::Mat image); /// Used to set the image to be viewed
-
+    
+    public slots:
+    bool    showImage(const cv::Mat& image); /// Used to set the image to be viewed
+    
 protected:
     void 	initializeGL(); /// OpenGL initialization
     void 	paintGL(); /// OpenGL Rendering
     void 	resizeGL(int width, int height);        /// Widget Resize Event
-
+    
     void        updateScene();
     void        renderImage();
-
+    
 private:
-    bool        mSceneChanged;          /// Indicates when OpenGL view is to be redrawn
-
+    
     QImage      mRenderQtImg;           /// Qt image to be rendered
     cv::Mat     mOrigImage;             /// original OpenCV image to be shown
-
+    
     QColor      mBgColor;		/// Background color
-
-    int         mOutH;                  /// Resized Image height
-    int         mOutW;                  /// Resized Image width
+    
     float       mImgRatio;             /// height/width ratio
-
-    int         mPosX;                  /// Top left X position to render image in the center of widget
-    int         mPosY;                  /// Top left Y position to render image in the center of widget
-
 };
 
 #endif // CQTOPENCVVIEWERGL_H
